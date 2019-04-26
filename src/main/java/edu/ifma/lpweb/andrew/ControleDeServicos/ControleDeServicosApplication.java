1fcd95442cf31c4a54e8c9caaa6df583af8a177c
andrewnrs <andrewnrs@outlook.com>
2019-04-26 18:05:59 -0300

    Inicio do projeto
    Commit inicial com o Spring Init.

---
+++ src/main/java/edu/ifma/lpweb/andrew/ControleDeServicos/ControleDeServicosApplication.java
@@ -0,0 +1,13 @@
+package edu.ifma.lpweb.andrew.ControleDeServicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeServicosApplication.class, args);
+	}
+
+}

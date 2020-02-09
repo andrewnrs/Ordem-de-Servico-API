f06648b37974e73aac7bbc9d4d3992b302c7451a
Andrew Silva <andrewnrs@outlook.com>
2020-02-09 12:22:26 -0300

    Ajuste na classe test

--- src/test/java/edu/ifma/lpweb/andrew/ControleDeServicos/ControleDeServicosApplicationTests.java
+++ src/test/java/edu/ifma/lpweb/andrew/ControleDeServicos/ControleDeServicosApplicationTests.java
@@ -1,21 +1,15 @@
 package edu.ifma.lpweb.andrew.ControleDeServicos;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ControleDeServicosApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
-	@Test
-	public void inicio(){
-		Assert.isTrue(true, "Certo");
-	}
 }
